d71fd3765e846704c44f746bc85bf24541a92e4c
Joshua <guyot.joshua@gmail.com>
2026-01-25 13:17:06 +0100

    Adding first part of the OpenMetrics server

--- WifiMonitor/src/main.rs
+++ WifiMonitor/src/main.rs
@@ -5,57 +5,82 @@ use reqwest::Client;
 use url::Url;
 use prometheus_client::encoding::{EncodeLabelSet, EncodeLabelValue};
 use prometheus_client::encoding::text::encode;
-use prometheus_client::metrics::counter::{Atomic, Counter};
+use prometheus_client::metrics::counter::{ Counter};
 use prometheus_client::metrics::family::Family;
 use prometheus_client::registry::Registry;
 use std::io::Write;
+use std::sync::Mutex;
+use actix_web::middleware::Compress;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result as ActixResult};
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelValue)]
+pub enum Method {
+    Get,
+    Post,
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
+pub struct MethodLabels {
+    pub method: Method,
+}
+
+pub struct Metrics {
+    requests: Family<MethodLabels, Counter>,
+}
+
+impl Metrics {
+    pub fn update_metrics(&self, method: Method) {
+        self.requests.get_or_create(&MethodLabels { method }).; //TODO Set download data to metrics -> Learn OpenMetrics process
+    }
+}
+
+pub struct AppState {
+    pub registry: Registry,
+}
+
+pub async fn metrics_handler(state: web::Data<Mutex<AppState>>) -> actix_web::Result<HttpResponse> {
+    let state = state.lock().unwrap();
+    let mut body = String::new();
+    encode(&mut body, &state.registry).unwrap();
+    Ok(HttpResponse::Ok()
+        .content_type("application/openmetrics-text; version=1.0.0; charset=utf-8")
+        .body(body))
+}
 
 #[tokio::main]
 async fn main() {
-    let mut registry = <Registry>::default();
-
-    #[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
-    struct Labels {
-        method: Method,
-        download: String,
-    };
-
-    #[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelValue)]
-    enum Method {
-        GET,
-        PUT,
-    };
-
-    let http_requests = Family::<Labels, Counter>::default();
-    registry.register(
-        "http_requests",
-        "Number of HTTP requests received",
-        http_requests.clone(),
-    );
     let now = SystemTime::now();
     tokio::task::spawn_blocking(|| download()).await;
     match now.elapsed() {
         Ok(elapsed) => {
-            println!("Duration {}", elapsed.as_secs());
             let float: f32 = (100.0/elapsed.as_secs_f32())*8.0;
             println!("Download: {float} Mb/s", );
-            http_requests.get_or_create(
-                &Labels { method: Method::GET, download: float.to_string() }
-            ).inc();
-        }
+                    }
         Err(e) => {
             println!("Back to the futur {e:?}");
         }
     }
+    let metrics = web::Data::new(Metrics {
+        requests: Family::default(),
+    });
+    let mut state = AppState {
+        registry: Registry::default(),
+    };
+    state
+        .registry
+        .register("download_link", "Download speed", metrics.requests.clone());
+    let state = web::Data::new(Mutex::new(state));
 
-    let mut buffer = String::new();
-    encode(&mut buffer, &registry).unwrap();
-
-    let expected = "# HELP http_requests Number of HTTP requests received.\n".to_owned() +
-        "# TYPE http_requests counter\n" +
-        "http_requests_total{method=\"GET\",path=\"/metrics\"} 1\n" +
-        "# EOF\n";
-    assert_eq!(expected, buffer);
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Compress::default())
+            .app_data(metrics.clone())
+            .app_data(state.clone())
+            .service(web::resource("/metrics").route(web::get().to(metrics_handler)))
+    })
+        .bind(("127.0.0.1", 8080)).expect("Failed to bind")
+        .run()
+        .await.expect("Http server error");
     //Curl https://speed.cloudflare.com/__down?bytes=100000000 --> Get start time / get endtime = duration fore 100mb
 }
 fn download() -> Result<()> {
